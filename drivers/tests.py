410e00b6ca003cb15024d17ed65bcc369ab929f3
Christian Horna <christian1020@gmail.com>
2021-07-10 17:56:22 -0500

    TDD cabs

--- drivers/tests.py
+++ drivers/tests.py
@@ -21,6 +21,27 @@ class DriverTests(APITestCase):
         self.driver_1 = self.create_helper(Driver, driver1)
         self.driver_2 = self.create_helper(Driver, driver2)
 
+        vehicle1 = {
+            'number_plate': 'kl-897',
+        }
+        self.vehicle_1 = self.create_helper(Vehicle, vehicle1)
+        vehicle2 = {
+            'number_plate': 'ab-234',
+        }
+        self.vehicle_2 = self.create_helper(Vehicle, vehicle2)
+
+        cab1_data = {
+            'driver_id': self.driver_1,
+            'vehicle_id': self.vehicle_1,
+            'state': 0,
+        }
+        self.cab1 = self.create_helper(Cab, cab1_data)
+        cab2_data = {
+            'driver_id': self.driver_2,
+            'vehicle_id': self.vehicle_2,
+        }
+        self.cab2 = self.create_helper(Cab, cab2_data)
+
         self.client = APIClient()
 
     def create_helper(self, model=None, d={}):
@@ -36,3 +57,19 @@ class DriverTests(APITestCase):
         # test get driver by id
         response2 = self.client.get(path+'%s/' % self.driver_1.id)
         self.assertEquals(status.HTTP_200_OK, response2.status_code)
+
+    def test_cabs(self):
+        path = '/api-v1.0/cab/'
+        # test get all cabs
+        response = self.client.get(path)
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+        self.assertEquals(len(response.json()), 2)
+
+        # test get cab by id
+        cab = self.client.get(f"{path}{self.cab2.id}/")
+        self.assertEquals(status.HTTP_200_OK, cab.status_code)
+
+        # test get all cabs availables
+        availables = self.client.get(f"{path}availables/")
+        self.assertEquals(status.HTTP_200_OK, availables.status_code)
+        self.assertEquals(len(availables.json()), 1)
